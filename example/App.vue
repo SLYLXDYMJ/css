4d605720d310032c9007620668066f3bbdedd539
q-jason <596908546@qq.com>
2019-09-12 10:49:28 +0800

    还差文档部分

--- example/App.vue
+++ example/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1 class="flex align-items-center-xs-only">asd</h1>
+    <h3 class="item">123</h3>
   </div>
 </template>
 
@@ -13,12 +14,8 @@
 <style lang="scss" scoped>
   @import "./styles/util";
   .item {
+    @extend %hidden-md-only;
     display: block;
     margin: 20px;
-    @include res(font-size, 50px, (
-      md: 40px,
-      sm: 30px,
-      xs: 20px
-    ))
   }
 </style>
--- src/_util/extend/README.md
+++ src/_util/extend/README.md
@@ -6,3 +6,15 @@
  ---------- | --------------------------------------
  clearfix   | 清除浮动影响，占用 :after
  ellipsis   | 使文字一行显示，溢出 ... 显示
+ hidden     | 响应式隐藏，下面有例子，responsive 专有
+
+### hidden
+> 名称和 layout-hidden 一致
+
+```scss
+.any {
+  @extend %hidden-md-up;
+  @extend %hidden-md-down;
+  @extend %hidden-md-only;
+}
+```
